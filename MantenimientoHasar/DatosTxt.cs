d9735427cc7bd39874c84be28f7e332175cf2bb1
FedericoArnaudo <federicoarnaudo.97@gmail.com>
2024-06-26 01:35:44 -0300

    Inicio Mantenimiento Hasar

---
+++ MantenimientoHasar/DatosTxt.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace MantenimientoHasar
+{
+    internal class DatosTxt
+    {
+        private readonly string baseDirectory;
+        private readonly string directoryPath;
+        public static readonly string fileProy = "Ruta.txt";
+        public static readonly string fileDelete = "Archivos_A_Borrar.txt";
+        private readonly string filePathProy;
+        private readonly string filePathDelete;
+        private static readonly DatosTxt instance = new DatosTxt();
+        private DatosTxt()
+        {
+            // Obtener el directorio de ejecución del programa
+            baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Combinar el directorio base con el nombre del directorio especificado
+            directoryPath = Path.Combine(baseDirectory, "Ruta_Y_Archivos_A_Borrar");
+
+            // Combinar el directorio de destino con el nombre del archivo especificado
+            filePathProy = Path.Combine(directoryPath, fileProy);
+
+            // Combinar el directorio de destino con el nombre del archivo especificado
+            filePathDelete = Path.Combine(directoryPath, fileDelete);
+        }
+        public static DatosTxt InstanciaDatosTxt()
+        {
+            return instance;
+        }
+        public void VerificarCarpeta()
+        {
+            // Verificar si la carpeta existe, y si no, crearla
+            if (!Directory.Exists(directoryPath))
+            {
+                _ = Directory.CreateDirectory(directoryPath);
+            }
+            VerificarArchivoRuta();
+            VerificarArchivoDeEliminacion();
+        }
+        public void VerificarArchivoRuta()
+        {
+            // Verificar si el archivo existe, y si no, crearlo
+            if (!File.Exists(filePathProy))
+            {
+                // Crear el archivo vacío
+                using (File.Create(filePathProy)) { }
+            }
+        }
+        public void VerificarArchivoDeEliminacion()
+        {
+            // Verificar si el archivo existe, y si no, crearlo
+            if (!File.Exists(filePathDelete))
+            {
+                // Crear el archivo vacío
+                using (File.Create(filePathDelete)) { }
+
+                File.WriteAllText(filePathDelete, "Agregue los archivos a Eliminar:");
+            }
+        }
+        public void Escribir(string archivo, string line)
+        {
+            bool contiene = false;
+            string filePath = "";
+
+            switch (archivo)
+            {
+                case "Ruta.txt":
+                    filePath = filePathProy;
+                    break;
+                case "Archivos_A_Borrar.txt":
+                    filePath = filePathDelete;
+                    break;
+                default:
+                    break;
+            }
+
+            // Leer todo el contenido del archivo si existe
+            string[] existingLines = File.Exists(filePath) ? File.ReadAllLines(filePath) : new string[0];
+
+            // Crear una lista para almacenar las nuevas líneas
+            List<string> lines = new List<string>();
+
+            foreach (string s in existingLines)
+            {
+                if (line.Equals(s))
+                {
+                    contiene = true;
+                }
+            }
+
+            if (contiene == false)
+            {
+                // Añadir las líneas existentes al final de la nueva lista
+                lines.AddRange(existingLines);
+                lines.Add(line);
+
+                // Escribir todas las líneas de vuelta en el archivo
+                File.WriteAllLines(filePath, lines);
+            }
+        }
+        public List<string> Listar(string archivo)
+        {
+            List<string> archivos = new List<string>();
+            string filePath = "";
+
+            switch (archivo)
+            {
+                case "Ruta.txt":
+                    filePath = filePathProy;
+                    break;
+                case "Archivos_A_Borrar.txt":
+                    filePath = filePathDelete;
+                    break;
+                default:
+                    break;
+            }
+            // Leer todo el contenido del archivo si existe
+            string[] existingLines = File.Exists(filePath) ? File.ReadAllLines(filePath) : new string[0];
+
+            archivos.AddRange(existingLines);
+
+            return archivos;
+        }
+        public void EliminarArchivos(string ruta)
+        {
+            List<string> archivos = Listar(fileDelete);
+            try
+            {
+                // Verifica si el directorio existe
+                if (Directory.Exists(ruta))
+                {
+                    // Obtén la lista de carpetas en el directorio
+                    List<string> carpetas = ObtenerCarpetas(ruta);
+
+                    for (int i = 0; i < archivos.Count; i++)
+                    {
+                        if (i > 0)
+                        {
+                            string archivo = archivos[i];
+                            // Borra el Dll
+                            foreach (string carpeta in carpetas)
+                            {
+                                Borrar(carpeta, archivo);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    _ = MessageBox.Show($"El directorio {ruta} no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show($"Error al obtener las carpetas: {ex.Message}");
+            }
+        }
+        private List<string> ObtenerCarpetas(string rutaDirectorio)
+        {
+            // Utiliza la clase Directory para obtener la lista de carpetas
+            string[] carpetasArray = Directory.GetDirectories(rutaDirectorio);
+
+            // Convierte el array en una lista
+            List<string> carpetas = new List<string>(carpetasArray);
+
+            return carpetas;
+        }
+        private void Borrar(string carpeta, string nombreArchivo)
+        {
+            // Obtener todos los archivos en el directorio
+            string[] archivos = Directory.GetFiles(carpeta);
+
+            foreach (string archivo in archivos)
+            {
+                // Verifica si el archivo existe
+                if (Path.GetFileName(archivo).StartsWith(nombreArchivo.Substring(0, 8).ToLower()) || Path.GetFileName(archivo).Equals(nombreArchivo))
+                {
+                    // Borra el archivo
+                    File.Delete(archivo);
+                }
+            }
+        }
+    }
+}
---
+++ MantenimientoHasar/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace MantenimientoHasar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NotifyIcon notifyIcon;
+        private readonly DispatcherTimer dispatcherTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Icon = new BitmapImage(new Uri("pack://application:,,,/MantenimientoHasar;component/MantenimientoHasar.ico"));
+            SetupNotifyIcon();
+            dispatcherTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromHours(2)
+            };
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Start();
+        }
+        private void btnComenzarMantenimiento_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(TextBoxRutaProyecto.Text == null || TextBoxRutaProyecto.Text == ""))
+            {
+                DatosTxt.InstanciaDatosTxt().VerificarCarpeta();
+                DatosTxt.InstanciaDatosTxt().Escribir(DatosTxt.fileProy, TextBoxRutaProyecto.Text);
+                DatosTxt.InstanciaDatosTxt().Escribir(DatosTxt.fileDelete, TextBoxArchivo.Text);
+            }
+            else
+            {
+                _ = MessageBox.Show($"Debe ingresar la ruta del proyecto");
+            }
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            List<string> archivos = DatosTxt.InstanciaDatosTxt().Listar(DatosTxt.fileProy);
+            if (archivos.Count == 1)
+            {
+                // Aquí se ejecuta el método cada vez que el DispatcherTimer hace tick
+                ExecuteMethod(archivos[0]);
+            }
+        }
+        private void ExecuteMethod(string ruta)
+        {
+            DatosTxt.InstanciaDatosTxt().EliminarArchivos(ruta);
+
+        }
+        private void btnBuscarRuta_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.MyComputer, // Carpeta raíz (opcional)
+                Description = "Selecciona una carpeta" // Descripción del diálogo (opcional)
+            };
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+                TextBoxRutaProyecto.Text = folderPath;
+            }
+        }
+        private void SetupNotifyIcon()
+        {
+            notifyIcon = new NotifyIcon
+            {
+                Icon = new Icon("MantenimientoHasar2.ico"),
+                Visible = true,
+                Text = "Mantenimiento Hasar"
+            };
+
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+
+            ContextMenu contextMenu = new ContextMenu();
+            _ = contextMenu.MenuItems.Add("Restaurar", (s, e) => RestoreFromTray());
+            _ = contextMenu.MenuItems.Add("Salir", (s, e) => ExitApplication());
+
+            notifyIcon.ContextMenu = contextMenu;
+        }
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            RestoreFromTray();
+        }
+        private void RestoreFromTray()
+        {
+            Show();
+            WindowState = WindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+        private void ExitApplication()
+        {
+            notifyIcon.Visible = false;
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
